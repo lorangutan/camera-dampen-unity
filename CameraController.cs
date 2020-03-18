c97988568986fb6f88a1ce85e9522c437ec3923f
lorangutan <62343039+lorangutan@users.noreply.github.com>
2020-03-18 21:37:40 +0700

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Follower = null;
+    [SerializeField]
+    private Transform Target = null;
+    [SerializeField]
+    private float dampFollow = 0;
+
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPos = Target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, dampFollow);
+
+        //to restrict the follower from moving in an axis just pass 0 as the parameter
+        //for 2D unity user Follower.position.z as the third parameter instead of smoothedPos.z
+        smoothedPos.Set(smoothedPos.x,smoothedPos.y, smoothedPos.z);
+
+        transform.position = smoothedPos;
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+To set up a camera that smoothly followed an object, drag the camera to Follower in the inspector, as well as the target obejct to the Object field.
+
+You can use this for other objects outside camera too.
